ece67e41ef4840ea94489efb6bd41882ad8043d5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2025-09-13 16:16:45 +0530

    Quick update

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-50 w-full pb-2 shadow-md" style={{background: theme.colors.background}} >
       <div className="flex items-center px-6 py-3">
-        <h1 className="text-lg font-semibold" style={{ color: theme.colors.primary }}>Custom Data Grid</h1>
+        <h1 className="ml-20 text-lg font-semibold" style={{ color: theme.colors.primary }}>Custom Data Grid</h1>
       </div>
     </header>
   );
